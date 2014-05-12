9ef4b379f83ce9ce51458e32ad5bc06cff4bf485
James Perez <xoptoss@gmail.com>
2014-05-11 21:52:17 -0700

    Update main.java

--- main.java
+++ main.java
@@ -3,11 +3,9 @@ public class main {
 
 	public static void main(String[] args) {
 
-		dice Dice = new dice(3,0);
-		tripleDice Dice2 = new tripleDice(4,0);
-		Dice.Throw();
-		Dice2.Throw();
-
+		dice Dice = new dice(3,4);
+		tripleDice Dice2 = new tripleDice(4,5);
+		System.out.println(Dice.Throw());	
+		System.out.println(Dice2.Throw());
 	}
-
 }
