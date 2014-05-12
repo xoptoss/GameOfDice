637bdf6a14fc4496b3e9480583044631db69f90a
James Perez <xoptoss@gmail.com>
2014-05-11 21:51:29 -0700

    Update dice.java

--- dice.java
+++ dice.java
@@ -3,15 +3,15 @@ public class dice {
 
 	public int bounces;
 	public int value;
-
+	
 	public dice(int Bounces, int Value)
 	{
 		bounces = Bounces;
 		value = Value;
 	}
-	public void Throw() {		
+	public int Throw() {		
 		Random random = new Random();		
 			value = random.nextInt(6)+1;  		
-		System.out.println("You Rolled :"+value);
+	return value;
 	}
 }
